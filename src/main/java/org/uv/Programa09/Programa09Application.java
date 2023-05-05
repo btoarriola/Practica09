5d15540a5062bc46ef0dd6ea99cba569535c9cbe
btoarriola <betokanon@gmail.com>
2023-05-05 10:20:11 -0500

    My first commit

---
+++ src/main/java/org/uv/Programa09/Programa09Application.java
@@ -0,0 +1,13 @@
+package org.uv.Programa09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programa09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Programa09Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/Programa09/Programa09ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.Programa09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Programa09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
